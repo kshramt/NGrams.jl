a47d087f7d132f7efa85a810a57806d08e4112f1
kshramt <thisisdummy@example.com>
2016-02-27 15:34:57 +0900

    Rename `ngram` -> `words`

--- src/NGrams.jl
+++ src/NGrams.jl
@@ -8,9 +8,9 @@ end
 NGramNode(word, count::Int) = NGramNode(word, count, Vector{NGramNode}())
 NGramNode(word) = NGramNode(word, 0)
 
-function NGramNode{T}(word, ngrams::Vector{Vector{T}})
-    ret = NGramNode(word, length(ngrams))
-    store_ngrams!(ret.children, ngrams)
+function NGramNode{T}(word, wordss::Vector{Vector{T}})
+    ret = NGramNode(word, length(wordss))
+    store_ngrams!(ret.children, wordss)
     ret
 end
 
@@ -38,24 +38,24 @@ function NGram(s::AbstractString, n)
 end
 
 
-function bundle{T}(ngrams::Vector{Vector{T}})
+function bundle{T}(wordss::Vector{Vector{T}})
     ret = Dict{T, Vector{Vector{T}}}()
-    for ngram in ngrams
-        w1 = ngram[1]
+    for words in wordss
+        w1 = words[1]
         if haskey(ret, w1)
-            push!(ret[w1], ngram[2:end])
+            push!(ret[w1], words[2:end])
         else
             # todo: cleaner way?
-            ret[w1] = push!(Vector{Vector{T}}(), ngram[2:end])
+            ret[w1] = push!(Vector{Vector{T}}(), words[2:end])
         end
     end
     ret
 end
 
 
-function store_ngrams!(children, ngrams)
-    if length(ngrams[1]) > 0
-        for (head, more) in bundle(ngrams)
+function store_ngrams!(children, wordss)
+    if length(wordss[1]) > 0
+        for (head, more) in bundle(wordss)
             push!(children, NGramNode(head, more))
         end
     end
